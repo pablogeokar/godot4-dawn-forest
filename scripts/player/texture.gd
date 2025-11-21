db5b0a33bf8bb8a6657d44e6dfe8f7739aa999f3
Pablo George <pablogeokar@hotmail.com>
2025-11-21 17:58:57 -0300

    adicionando animações ao pular

--- scripts/player/texture.gd
+++ scripts/player/texture.gd
@@ -2,12 +2,16 @@ extends Sprite2D
 class_name PlayerTexture
 
 @onready var animation: AnimationPlayer = $"../Animation"
+@onready var player: CharacterBody2D = $".."
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
 	
 	if direction.y != 0:
 		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("landing")
+		player.set_physics_process(false)
 	else:
 		horizontal_behavior(direction)
 	
@@ -25,7 +29,16 @@ func horizontal_behavior(direction: Vector2) -> void:
 		
 func vertical_behavior(direction: Vector2) -> void:
 	if direction.y > 0:
+		player.landing = true
 		animation.play("fall")
 	elif direction.y < 0:
 		animation.play("jump")
 	pass
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"landing":
+			player.landing = false
+			player.set_physics_process(true)
+	
