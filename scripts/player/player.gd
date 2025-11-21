6d03beef6acdc937777e762b22f28009d9d16888
Pablo George <pablogeokar@hotmail.com>
2025-11-21 17:46:13 -0300

    animações do personagem

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,13 +3,34 @@ class_name Player
 
 @onready var player_sprite: Sprite2D = $Texture
 @export var speed: int = 150
+@export var jump_speed: int = 150
+@export var player_gravity: int = 150
 
-func _physics_process(_delta: float) -> void:
+var jump_count: int = 0
+var landing: bool = false
+
+func _physics_process(delta: float) -> void:
 	horizontal_movement_env()
+	vertical_movement_env()
+	gravity(delta)
 	move_and_slide()
 	player_sprite.animate(velocity)
 	
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.x = input_direction * speed
+
+func vertical_movement_env() -> void:
+	if is_on_floor():
+		jump_count = 0
+		
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+		jump_count += 1
+		velocity.y = jump_speed		
+	
+
+func gravity(delta: float) -> void:
+	velocity.y += delta * player_gravity
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
 	
--- scripts/player/texture.gd
+++ scripts/player/texture.gd
@@ -5,7 +5,11 @@ class_name PlayerTexture
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	horizontal_behavior(direction)
+	
+	if direction.y != 0:
+		vertical_behavior(direction)
+	else:
+		horizontal_behavior(direction)
 	
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
@@ -18,3 +22,10 @@ func horizontal_behavior(direction: Vector2) -> void:
 		animation.play("run")		
 	else:
 		animation.play("Idle")
+		
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		animation.play("fall")
+	elif direction.y < 0:
+		animation.play("jump")
+	pass
